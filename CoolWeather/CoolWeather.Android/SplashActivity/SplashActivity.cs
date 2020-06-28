de6411495df522dee6f616ae3f89074af6c7f37d
Ricardo Brito Brens <britobrensricardo@gmail.com>
2020-06-28 15:07:09 -0400

    Initial Commit

---
+++ CoolWeather/CoolWeather.Android/SplashActivity/SplashActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Com.Airbnb.Lottie;
+
+namespace CoolWeather.Droid.SplashActivity
+{
+    [Activity(Label = "SplashScreen",
+        Theme = "@style/CoolWeather.SplashScreenWeatherAnimationStyle",
+        MainLauncher = true,
+        NoHistory = true)]
+    public class SplashActivity : Activity, Animator.IAnimatorListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.WeatherAnnimationSplashScreenLayout);
+
+            var animationView = FindViewById<LottieAnimationView>(Resource.Id.SplashScreenWeatherAnimation);
+            animationView.AddAnimatorListener(this);
+            animationView.RepeatCount = 2;
+            animationView.PlayAnimation();
+        }
+        public void OnAnimationCancel(Animator animation)
+        {
+
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+
+        }
+    }
+}
---
+++ WeatherAppNotes.md
@@ -0,0 +1,33 @@
+# Weather App Notes
+
+## Elements identifye
+
+* WeatherText
+* WeatherIcon
+* Source
+* IsDayTime
+* Temperature
+* RelativeHumidity
+* WindSpeed
+* UVIdenx
+* Pressure
+* Past6HourMin
+* Past6HourMax
+* TimeStamp
+
+Cold:
+* Top: bce1fc
+* Buttom: a7cbfd
+  
+Warm:
+* Top: f6ca69
+* Buttom: fca97f
+
+Night:
+* Top: 1a2d47
+*  Buttom: 365876
+
+
+https://www.uplabs.com/posts/weather-app-f41080cc-063a-499b-ad9c-18936575a5ac
+
+https://www.youtube.com/watch?v=dg9-Fi8G85E&t=272s
